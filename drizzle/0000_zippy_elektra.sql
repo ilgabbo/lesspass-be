ffe45349275033eb0b88b75f75b64e5dfa38d4b1
ilgabbo805 <f.mancini.gabriele@gmail.com>
2025-05-25 20:36:47 +0200

    drizzle folder in gitignore, .env.example

--- drizzle/0000_zippy_elektra.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "user" (
-	"userId" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"firstName" varchar(25) NOT NULL,
-	"lastName" varchar(25) NOT NULL,
-	"email" varchar(50) NOT NULL,
-	"password" varchar NOT NULL
-);
--- drizzle/0002_silly_thundra.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "folders" DROP CONSTRAINT "folders_parentId_folders_folderId_fk";
---> statement-breakpoint
-ALTER TABLE "folders" ADD CONSTRAINT "folders_parentId_folders_folderId_fk" FOREIGN KEY ("parentId") REFERENCES "public"."folders"("folderId") ON DELETE cascade ON UPDATE no action;
